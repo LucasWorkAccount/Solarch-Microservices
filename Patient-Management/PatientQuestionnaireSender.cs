22c9ccec79efaa3d122f35f420544b87b21944cb
Matthijs Feringa <mt.feringa@student.avans.nl>
2024-06-17 16:46:49 +0200

    added patientManagment

---
+++ Patient-Management/PatientQuestionnaireSender.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+
+namespace Patient_Management;
+
+public class PatientQuestionnaireSender
+{
+    private ConnectionFactory _factory = new();
+
+    public void QuestionnaireSender()
+    {
+        _factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+        
+    }
+}
---
+++ PatientManagement/PatientQuestionnaire.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+
+namespace PatientManagement;
+
+public class PatientQuestionnaire
+{
+    private ConnectionFactory _connectionFactory;
+
+    PatientQuestionnaire()
+    {
+        _connectionFactory = new ConnectionFactory();
+    }
+
+    public void Sender()
+    {
+        _connectionFactory.Uri = new Uri("amqp://guest:guest@localhost:4420");
+        _connectionFactory.ClientProvidedName = "Patient Questionnaire Sender App";
+
+        using var connection = _connectionFactory.CreateConnectionAsync();
+        //using var channel = connection.CreationOptions();
+    }
+}
