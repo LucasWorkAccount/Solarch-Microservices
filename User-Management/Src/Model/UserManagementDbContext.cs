0e29a6112cff439a33ae954424bf0e4b0fef2633
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2024-06-19 15:54:30 +0200

    Create new project for user management within the solution

---
+++ User-Management/Src/Model/UserManagementDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace User_Management.Model;
+
+public partial class UserManagementDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    
+    public UserManagementDbContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public UserManagementDbContext(DbContextOptions<UserManagementDbContext> options, IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        => optionsBuilder.UseNpgsql(_configuration.GetConnectionString("user-management-db-npgsql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .HasColumnName("email");
+            entity.Property(e => e.Password)
+                .HasMaxLength(100)
+                .HasColumnName("password");
+            entity.Property(e => e.Role)
+                .HasMaxLength(100)
+                .HasColumnName("role");
+            entity.Property(e => e.Uuid).HasColumnName("uuid");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
