d89b0767f7f8fad75da54f5c8f158755e503f1c5
Jaron <jaronvwell@gmail.com>
2024-06-20 15:10:11 +0200

    Remove spaces

--- User-Management/Src/JWTGenerator.cs
+++ User-Management/Src/JWTGenerator.cs
@@ -1,8 +1,5 @@
-﻿
-
-using System.IdentityModel.Tokens.Jwt;
+﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Text;
 using Microsoft.IdentityModel.Tokens;
 
 namespace User_Management.Model;
