42592a3e5bfa3aa3032ffa4663d8c307d781d8b1
Lucas de Kleijn <l.dekleijn2@student.avans.nl>
2024-06-21 14:09:56 +0200

    Add endpoints for creating and planning appointments

---
+++ Appointment-Planner/Src/Entities/Appointment.cs
@@ -0,0 +1,25 @@
+﻿namespace Appointment_Planner.Entities;
+
+public partial class Appointment
+{
+    public Appointment(Guid patient, Guid doctor, DateTime datetime, string arrival, Guid referral)
+    {
+        Patient = patient;
+        Doctor = doctor;
+        Datetime = datetime;
+        Arrival = arrival;
+        Referral = referral;
+    }
+    
+    public int Id { get; set; }
+
+    public Guid Patient { get; set; }
+
+    public Guid Doctor { get; set; }
+
+    public DateTime Datetime { get; set; }
+
+    public string Arrival { get; set; } = null!;
+
+    public Guid Referral { get; set; }
+}
