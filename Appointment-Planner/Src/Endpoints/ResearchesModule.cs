d66298d3e52a44373004f1acfd88000263ae6608
Jaron <jaronvwell@gmail.com>
2024-06-22 14:24:56 +0200

    Move endpoints to module classes

---
+++ Appointment-Planner/Src/Endpoints/ResearchesModule.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Appointment_Planner.Entities;
+using Appointment_Planner.Repositories;
+
+namespace Appointment_Planner.Endpoints;
+
+public static class ResearchesModule
+{
+    public static IEndpointRouteBuilder MapResearchEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapPost("/research-results",
+                (ResearchResults researchResults, IGeneralPractitionerResultsSenderService sender) =>
+                {
+                    try
+                    {
+                        sender.Send("General-practitioner-results", JsonSerializer.Serialize(researchResults));
+                        return Results.Json(new
+                        {
+                            status = 200,
+                            message = "Email sent to general practitioner successfully!"
+                        });
+                    }
+                    catch
+                    {
+                        return Results.Json(new
+                        {
+                            status = 400,
+                            message = "Failed to send email to general practitioner!"
+                        });
+                    }
+                })
+            .WithName("SendResearchResults")
+            .WithOpenApi();
+
+        return endpoints;
+    }
+}
